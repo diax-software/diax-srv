97789dd65864a876fdbfa2a6f8531c5078182888
Sven Olderaan <admin@heaven-craft.net>
2017-04-12 20:09:04 +0200

    Initial commit for the srv

---
+++ diax-srv-app/src/main/java/me/diax/srv/Main.java
@@ -0,0 +1,12 @@
+package me.diax.srv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().main();
+    }
+
+    public void main() {
+        System.out.println("Hello world!");
+    }
+}
