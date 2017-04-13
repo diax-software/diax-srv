e96a52096b8b9118af56340ac7f6503eac3cee74
Sven Olderaan <s.olderaan@i-real.nl>
2017-04-13 16:12:39 +0200

    Removed some lombok calls

--- diax-srv-stubs/src/main/java/me/diax/srv/stubs/model/Profile.java
+++ diax-srv-stubs/src/main/java/me/diax/srv/stubs/model/Profile.java
@@ -1,14 +1,14 @@
 package me.diax.srv.stubs.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
 @XmlRootElement(name = "profile")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Profile extends IdentifiableModel {
--- diax-srv-stubs/src/main/java/me/diax/srv/stubs/service/ServiceException.java
+++ diax-srv-stubs/src/main/java/me/diax/srv/stubs/service/ServiceException.java
@@ -1,19 +1,19 @@
 package me.diax.srv.stubs.service;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 @XmlRootElement(name = "exception")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ServiceException extends Exception {
+    @Getter
+    @Setter
     private String message;
 
     public ServiceException(String message) {
